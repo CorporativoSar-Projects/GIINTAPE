1bbe73b9e690cf977871c518b93a451e1591c1be
EsmeContreras <esme.pcontreras@gmail.com>
2024-12-13 19:43:03 -0600

    Pruebas end to end

--- js/modalPrivacyNotice.js
+++ js/modalPrivacyNotice.js
@@ -1,6 +1,7 @@
 // Get the modal
 var modalAviso = document.getElementById("privacyModal");
 var btnTrabajo = document.getElementById("btnTrabajo");
+var labelJobCurrent = document.getElementById("labelJobCurrent");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
@@ -10,12 +11,14 @@ var span = document.getElementsByClassName("closeAviso")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
+    labelJobCurrent.style.display="none";
     modalAviso.style.display = "block";
     btnTrabajo.style.display= "none";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+    labelJobCurrent.style.display="block";
     modalAviso.style.display = "none";
     btnTrabajo.style.display= "block";
 }
@@ -25,5 +28,6 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modalAviso.style.display = "none";
     btnTrabajo.style.display= "block";
+    labelJobCurrent.style.display="block";
   }
 }
