24f37e9cd2f1ca6bc02f61af7011980b15c66494
Esmeralda Contreras <103052992+EsmeContreras@users.noreply.github.com>
2024-12-15 21:45:50 -0600

    Esmeralda rediseño (#35)
    * Actualización rama Esme
    
    * Cambio comentario
    
    * Prueba
    
    * prueba
    
    * Sprint 1 y 2
    
    * Sprint 3
    
    * Sprint 4
    
    * Sprint 4
    
    * Sprint 5
    
    * Sprint 6
    
    * Sprint 8
    
    * Sprint 8
    
    * Pruebas
    
    * Pruebas end to end
    
    ---------
    
    Co-authored-by: Brayan Paulino <150291136+PauliRTS@users.noreply.github.com>
    Co-authored-by: Giintape Innovahue <128183702+CorporativoSAR@users.noreply.github.com>

--- contact.php
+++ contact.php
@@ -3,7 +3,11 @@
   mb_internal_encoding('UTF-8');
   require("PHPMailer-master/src/PHPMailer.php");
   require("PHPMailer-master/src/SMTP.php");
-
+  $etiquetaTel = utf8_encode("Teléfono");
+  $etiquetaArea = utf8_encode("Área de interés");
+  $etiquetaSituacion = utf8_encode("Situación profesional");
+  $etiquetaTrabaja = utf8_encode("¿Trabaja actualmente?");
+  $etiquetaTipo = utf8_encode("Tipo de prestación");
   $firstName = utf8_encode($_POST['firstName']);
   $lastName = utf8_encode($_POST['lastName']);
   $inputEmail = utf8_encode($_POST['email']);
@@ -11,14 +15,14 @@
   $areaOfInterest = utf8_encode($_POST['AreaDeInteres']);
   $professionalSituation = utf8_encode($_POST['SituacionProfesional']) ;
   $typeOfService = utf8_encode($_POST['TipoDePrestacion']);
-  $workActually = isset($_POST['trabajo']) ? 'SI' : 'NO';
+  $workActually = isset($_POST['trabajo']) ? 'Si' : 'No';
   $cv = $_FILES['cv'];
   $correo=$_GET['c'];
   $mainEmail = "contacto@giintapeinnovahue.com";
 
-  $subject = $firstName . " " . $lastName . " desea unirse a nuestro equipo.";
+  $subject = utf8_decode($firstName) . " " . utf8_decode($lastName) . " nos envió su CV.";
 
-  $body = utf8_encode("<h2>Hola equipo Giintape!</h2>
+  $body = utf8_encode("<h2>Hola, equipo Giintape</h2>
   <p>Una persona quiere unirse a nuestro equipo de trabajo.</p>
   <p>Aquí los detalles:</p>");
 
@@ -36,19 +40,19 @@
   <p><b>- Nombre(s):</b> " . $firstName . "<br>
   <b>- Apellido(s):</b> " . $lastName . "<br>
   <b>- Correo:</b> " . $inputEmail . "<br>
-  <b>- Teléfono:</b> " . $inputPhone . "<br>
-  <b>- Área de interés:</b> " . $areaOfInterest . "<br>
-  <b>- Situación Profesional:</b> " . $professionalSituation . "<br>
-  <b>- Tipo de prestación:</b> " . $typeOfService . "<br>
-  <b>- ¿Trabaja actualmente?:</b> " . $workActually . "</p>
+  <b>- ".$etiquetaTel.":</b> " . $inputPhone . "<br>
+  <b>- ".$etiquetaArea.":</b> " . $areaOfInterest . "<br>
+  <b>- ".$etiquetaSituacion.":</b> " . $professionalSituation . "<br>
+  <b>- ".$etiquetaTipo.":</b> " . $typeOfService . "<br>
+  <b>- ".$etiquetaTrabaja.":</b> " . $workActually . "</p>
   <p>Por favor, contacta a este candidato para poder agendar una primera entrevista.</p>
-  <p style='font-size: 8pt;'><em>* Correo enviado a través del sitio web corporativo. No respondas a este correo.</em></p>");
+  <p style='font-size: 8pt; color:red;'><em>* Correo enviado mediante una automatizacion del sitio web. No respondas a este correo.</em></p>");
 
   $mail->Username = "contacto@giintapeinnovahue.com";
   $mail->Password = "giintap35$";
   $mail->SetFrom("contacto@giintapeinnovahue.com");
   $mail->Subject = $subject;
-  $mail->AddAddress("econtrerasp@giintapeinnovahueteam.onmicrosoft.com");
+  $mail->AddAddress("pfrattini@giintapeinnovahueteam.onmicrosoft.com");
 
   if(isset($cv) && $cv['error'] == 0) {
     $mail->AddAttachment($cv['tmp_name'], $cv['name']);
@@ -65,8 +69,6 @@
       $typeOfService = "";
       $workActually= "";
       $correo=0;
-      require_once("contact.html?c=0");
-
       require("modalTest.php");
     }
     else
--- modalTest.php
+++ modalTest.php
@@ -7,6 +7,48 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="e/promo.css">
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  
+  <!--Barra de Redes-->
+  <meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <!--Icon-Font-->
+  <script src="https://kit.fontawesome.com/c41cb6ddd6.js" crossorigin="anonymous"></script>
+  <!--End Barra de Redes-->
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <link rel="shortcut icon" href="icon/logogiintape.png">
+
+  <!-- bootstrap core css -->
+  <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.css" /> -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"></script>
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,700|Roboto:400,700&display=swap" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+
+  <!--Icon-Font-->
+  <script src="https://kit.fontawesome.com/c41cb6ddd6.js" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <style>
 	#id01{
@@ -18,8 +60,9 @@
 	}
 </style>
 <body>
-	<!-- Modal -->
-   <div id="id01" class="w3-modal">
+<img src="images/company.jpg" width="100%"/>
+	  <!-- Modal -->
+<div id="id01" class="w3-modal" style="margin-top:0 !important;">
       <div class="w3-modal-content">
          <div class="w3-container">
             <!-- Para minimizar ventana:
@@ -34,5 +77,155 @@
          </div>
       </div>
    </div>
+     <!--Barra Redes-->
+  <div class="container-bar">
+    <input type="checkbox" id="btn-social">
+    <label for="btn-social" class="fa fa-play"></label>
+    <div class="icon-social">
+      <a href="https://www.facebook.com/innsolcorporation" class="fa fa-facebook">
+        <span id="title">Facebook</span>
+      </a>
+      <a href="https://www.instagram.com/giintape_innovahue/" class="fa fa-instagram">
+        <span id="title">Instagram</span>
+      </a>
+      <a href="mailto:contacto@giintapeinnovahue.com" class=" fa fa-envelope">
+        <span id="title">Correo</span>
+      </a>
+      <a href="https://wa.me/525653975314" class="fa fa-whatsapp">
+        <span id="title">WhatsApp</span>
+      </a>
+    </div>
+  </div>
+  <!--End Barra Redes-->
+
+
+
+ <!-- footer section -->
+ <section class="info_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="images/logogiintape.png" width="200" height="200">
+          <br>
+          <br>
+          <div class="info-logo">
+            <h2>
+              GIINTAPE INNOVAHUE
+            </h2>
+            <p>
+              GRUPO IMPULSOR DE INNOVACIÓN EN TECNOLOGÍA Y AUTOMATIZACIÓN DE PROCESOS EMPRESARIALES INNOVAHUE, S.A.S.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4 nav-options">
+          <div class="info-nav">
+            <h4 style="font-size: 25px;">
+              Navegar
+            </h4>
+            <ul>
+              <li>
+                <a style="font-size: 23px"  href="index.html">
+                  Inicio
+                </a>
+              </li>
+
+              <!--
+              <li>
+                <a style="font-size: 23px" href="about.html">
+                  Informaci&oacute;n
+                </a>
+              </li>
+              <li>
+                <a style="font-size: 23px" href="consultoriamk.html">
+                  Marketing Digital
+                </a>
+              </li>
+              <li>
+                <a style="font-size: 23px" href="desarrolloweb.html">
+                  Desarrollo de Software
+                </a>
+              </li>
+              -->
+              <li>
+                <a style="font-size: 23px;" href="https://giintapeinnovahue.freshdesk.com/support/home">
+                  Soporte técnico
+                </a>
+              </li>
+              <li>
+                <a style="font-size: 23px" href="contact.html">
+                  Talento INNOVAHUE
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="info-contact">
+            <h4 style="font-size: 22px">
+              Información de Contacto
+            </h4>
+            <div class="location">
+              <h6 style="font-size: 20px; color: white;">
+                Nuestras Oficinas:
+              </h6>
+              <a href="https://maps.app.goo.gl/rjm6zeSu2Z2jMfmP9" target="_blank">
+                <img src="images/location.png" alt="">
+                <span style="font-size: 16px;">
+                  Río Guadiana 31, Cuauhtémoc, Cuauhtémoc, 06500, CDMX
+                </span>
+              </a>
+            </div>
+            <div class="call">
+              <h6 style="font-size: 20px; color: white;">
+                Tel&eacute;fono:
+              </h6>
+              <a>
+                <img src="images/telephone.png" alt="">
+                <span style="font-size: 16px;">
+                  (+52)56 5397 5314
+                </span>
+              </a>
+            </div>
+            <br>
+            <div class="email">
+              <h6 style="font-size: 20px; color: white;">
+                Correo:
+              </h6>
+              <a href="mailto:contacto@giintapeinnovahue.com" target="_blank">
+                <img src="images/telephone.png" alt="">
+                <span style="font-size: 16px;">
+                  contacto@giintapeinnovahue.com
+                </span>
+              </a>
+            </div>
+            <div class="time">
+              <h6 style="font-size: 20px; color: white;">
+                Horario:
+              </h6>
+              <a>
+                <img src="images/time.png" alt="">
+                <span style="font-size: 16px;">
+                  Lunes a viernes de 9 a 18 hrs.
+                </span><br>
+              </a>
+            </div>
+          </div>
+          <!--Links Redes Sociales-->
+          <ul class="sci">
+            <li><a href="https://www.facebook.com/innsolcorporation"><ion-icon name="logo-facebook"></ion-icon></a></li>
+            <li><a href="https://www.instagram.com/giintape_innovahue/"><ion-icon name="logo-instagram"></ion-icon></a></li>
+            <li><a href="https://es.linkedin.com/company/giintape-innovahue"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+            <!--<li><a href="https://www.tiktok.com/@mktikeip7"><ion-icon name="logo-tiktok"></ion-icon></a></li>-->
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end footer section -->
+   <!--Iconos redes sociales footer-->
+  
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
 </body>
 </html>
